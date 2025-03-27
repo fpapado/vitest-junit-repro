7caa45298233f007479dc99f88cdd4a0a8eaa4ea
Fotis Papadogeorgopoulos <fotis.papadogeorgopoulos@wolt.com>
2025-03-27 11:28:45 +0200

    Add fake warning

--- src/test-1.spec.js
+++ src/test-1.spec.js
@@ -2,6 +2,7 @@ import { expect, describe, it } from "vitest";
 
 describe("test-1", () => {
   it("passes", () => {
+    console.warn("WARNING: Fake warning");
     expect(true).toEqual(true);
   });
 });
