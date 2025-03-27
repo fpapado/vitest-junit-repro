8c9bab4e08ae017ac93c2c8d36d81e98d57f856f
Fotis Papadogeorgopoulos <fotis.papadogeorgopoulos@wolt.com>
2025-03-27 11:34:34 +0200

    Add log color

--- src/test-1.spec.js
+++ src/test-1.spec.js
@@ -1,8 +1,9 @@
 import { expect, describe, it } from "vitest";
+import { styleText } from "node:util";
 
 describe("test-1", () => {
   it("passes", () => {
-    console.warn("WARNING: Fake warning");
+    console.warn(styleText("yellow", "WARNING: fake warning"));
     expect(true).toEqual(true);
   });
 });
